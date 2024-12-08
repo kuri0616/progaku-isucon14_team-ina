bdab9714503517558134ca5c2d295c350585afa7
日下陸矢 <rikuya.98@gmail.com>
2024-12-08 17:06:56 +0900

    rideの型修正

--- home/isucon/webapp/go/app_handlers.go
+++ home/isucon/webapp/go/app_handlers.go
@@ -699,7 +699,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 		status = yetSentRideStatus.Status
 	}
 
-	fare, err := noTxCalculateDiscountedFare(ctx, db, user.ID, ride, ride.PickupLatitude, ride.PickupLongitude, ride.DestinationLatitude, ride.DestinationLongitude)
+	fare, err := rideCalculateDiscountedFare(ctx, db, user.ID, ride, ride.PickupLatitude, ride.PickupLongitude, ride.DestinationLatitude, ride.DestinationLongitude)
 	if err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
@@ -1098,6 +1098,44 @@ func noTxCalculateDiscountedFare(ctx context.Context, db *sqlx.DB, userID string
 			discount = coupon.Discount
 		}
 	}
+
+	//rideの型に合わせたver
+	func rideCalculateDiscountedFare(ctx context.Context, db *sqlx.DB, userID string, ride *Ride, pickupLatitude, pickupLongitude, destLatitude, destLongitude int) (int, error) {
+		var coupon Coupon
+		discount := 0
+		if ride != nil {
+			destLatitude = ride.DestinationLatitude
+			destLongitude = ride.DestinationLongitude
+			pickupLatitude = ride.PickupLatitude
+			pickupLongitude = ride.PickupLongitude
+	
+			// すでにクーポンが紐づいているならそれの割引額を参照
+			if err := db.GetContext(ctx, &coupon, "SELECT * FROM coupons WHERE used_by = ?", ride.RideId); err != nil {
+				if !errors.Is(err, sql.ErrNoRows) {
+					return 0, err
+				}
+			} else {
+				discount = coupon.Discount
+			}
+		} else {
+			// 初回利用クーポンを最優先で使う
+			if err := db.GetContext(ctx, &coupon, "SELECT * FROM coupons WHERE user_id = ? AND code = 'CP_NEW2024' AND used_by IS NULL", userID); err != nil {
+				if !errors.Is(err, sql.ErrNoRows) {
+					return 0, err
+				}
+	
+				// 無いなら他のクーポンを付与された順番に使う
+				if err := db.GetContext(ctx, &coupon, "SELECT * FROM coupons WHERE user_id = ? AND used_by IS NULL ORDER BY created_at LIMIT 1", userID); err != nil {
+					if !errors.Is(err, sql.ErrNoRows) {
+						return 0, err
+					}
+				} else {
+					discount = coupon.Discount
+				}
+			} else {
+				discount = coupon.Discount
+			}
+		}
 
 	meteredFare := farePerDistance * calculateDistance(pickupLatitude, pickupLongitude, destLatitude, destLongitude)
 	discountedMeteredFare := max(meteredFare-discount, 0)
